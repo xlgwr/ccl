fab9c8ca04a49ff2954943440b00aab05a369f29
xlgwr <xlgwr@163.com>
2015-04-21 10:33:34 +0800

    add rdesc for items

--- api/route/qadreportsurls.js
+++ api/route/qadreportsurls.js
@@ -34,9 +34,13 @@ exports.listAll=function(req,res) {
 
     var tmpsql='';
     if(subitem!=='All'){
-	tmpsql="select distinct dirname as name,leafname as value,'' as rdesc from dbo.Docs where dirname = 'QAD Reports/"+subitem+"'";
+	tmpsql="select distinct a.dirname as name,a.leafname as value,isnull(b.tp_ColumnSet.value('nvarchar7[1]','nvarchar(100)'),'') as rdesc from dbo.Docs a "
+	tmpsql=tmpsql+" left join dbo.UserData b on (a.listid=b.tp_ListId and a.DoclibRowId=b.tp_id) "
+	tmpsql=tmpsql+" where a.dirname = 'QAD Reports/"+subitem+"'";
     }else{
-	tmpsql="select distinct dirname as name,leafname as value,'' as rdesc from dbo.Docs where dirname like 'QAD Reports/%'";
+	tmpsql="select distinct a.dirname as name,a.leafname as value,isnull(b.tp_ColumnSet.value('nvarchar7[1]','nvarchar(100)'),'') as rdesc from dbo.Docs a "
+	tmpsql=tmpsql+" left join dbo.UserData b on (a.listid=b.tp_ListId and a.DoclibRowId=b.tp_id) "
+	tmpsql=tmpsql+" where a.dirname like 'QAD Reports/%'";
     }
     
     console.log(tmpsql);
